c3faa8eef986d2977b66d66ca0993b82cca7049a
David Öhlin <hello@davidohl.in>
2018-03-16 11:51:40 +0100

    add working sliders

--- wp-content/themes/blog/assets/dist/js/app.js
+++ wp-content/themes/blog/assets/dist/js/app.js
@@ -18,15 +18,14 @@ jQuery(function($) {
 	});
 
 
-	// get all containers
-	var galleryContainers = $(document).find('.post__img-section');
-	
-	for ( var i=0; i < galleryContainers.length; i++ ) {
-		var currentCarousel = $(galleryContainers[i]).find('.post__carousel');
-		var currentCarouselNext = $(galleryContainers[i]).find('.post__carousel-next');
-		var currentCarouselPrevious = $(galleryContainers[i]).find('.post__carousel-next');
-		console.log(currentCarousel);
-		$(currentCarousel).flickity({
+	var carousels = $('.post__img-section').has('.post__carousel');
+
+	$(carousels).each( function( i, container )  {
+		var carouselContainer = $(container).find('.post__carousel');
+		var next = $(container).find('.post__carousel-next');
+		var prev = $(container).find('.post__carousel-previous');
+
+		var carousel = $(carouselContainer).flickity({
 			// options
 			cellAlign: 'left',
 			contain: true,
@@ -35,15 +34,14 @@ jQuery(function($) {
 			wrapAround: true
 		});
 
-		$(currentCarouselNext).on( 'click', function() {
-			$(currentCarousel).flickity( 'next' );
+		$(next).on('click', function() {
+			carousel.flickity( 'next' );
 		});
 
-		$(currentCarouselPrevious).on( 'click', function() {
-			$(currentCarousel).flickity( 'previous' );
+		$(prev).on('click', function() {
+			carousel.flickity( 'previous' );
 		});
-	}
-
+	});
 
 	var $service = $('.service__carousel').flickity({
 		// options
@@ -53,6 +51,22 @@ jQuery(function($) {
 		pageDots: false,
 	});
 
+	// Add active to first element
+	$($service.data().flickity.cells[0].element).addClass('active');
+
+	$service.on('settle.flickity', function() {
+		var data = $service.data().flickity;
+		var slides = data.cells;
+		var index = data.selectedIndex;
+
+		console.log(data.cells);
+
+		for (var i = 0; i < data.cells.length; i++) {
+			i <= index ? 
+				$(slides[i].element).addClass('active') : 
+				$(slides[i].element).removeClass('active')
+		}
+	})
 
 
 });
