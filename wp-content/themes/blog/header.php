10a8e0b28ca3c8916fe9a955a55ff1a7e25efa16
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-04-12 17:02:22 +0200

    Add GT to project

--- wp-content/themes/blog/header.php
+++ wp-content/themes/blog/header.php
@@ -12,11 +12,23 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
+		<!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5SQQC9Z');</script>
+        <!-- End Google Tag Manager -->
+
+
 		<?php wp_head(); ?>
 
 	</head>
 	<body>
-
+	<noscript>
+	 	<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SQQC9Z"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
 
 	<header class="header container clearfix">
 		<div class="col-12 sm-col-12 md-col-11 lg-col-10 mx-auto">
